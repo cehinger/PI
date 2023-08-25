72be30dd91630c5128e45d4ceceb7b3b3425bd9c
Clément EHINGER <ehinger.clement@gmail.com>
2023-08-25 17:55:37 +0200

    strange things staying on github desktop

--- app/script.js
+++ app/script.js
@@ -13,10 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const stopIcon = document.getElementById("stop-icon");
   const progressContainer = document.querySelector(".progress-timer-container");
   const volumeSlider = document.querySelector(".volume-slider");
+  const volumeIcon = document.querySelector(".volume-icon img");
 
+  
   buttons.forEach(button => {
     button.addEventListener("click", () => {
       const soundName = button.getAttribute("data-sound");
+      const musicTitle = button.getAttribute("data-title");
+      const musicImage = button.getAttribute("data-image");
+      const musicArtist = button.getAttribute("data-artist");
 
       if (currentAudio) {
         currentAudio.pause();
@@ -30,6 +35,25 @@ document.addEventListener("DOMContentLoaded", () => {
       if (soundName) {
         currentAudio = new Audio(soundName);
         currentAudio.play();
+        document.getElementById("music-title").textContent = musicTitle;
+        document.getElementById("music-image").setAttribute("src", musicImage);
+        document.getElementById("music-artist").textContent = musicArtist;
+        
+        // Mettre à jour la classe hidden pour l'image
+        const musicImageElement = document.getElementById("music-image");
+        if (musicImage) {
+          musicImageElement.classList.remove("hidden");
+        } else {
+          musicImageElement.classList.add("hidden");
+        }
+
+        currentAudio.addEventListener("ended", () => {
+          isPlaying = false;
+          playIcon.style.display = "inline";
+          pauseIcon.style.display = "none";
+          progressBar.style.width = "0%";
+          timerDisplay.textContent = "0:00";
+        });
 
         currentAudio.addEventListener("timeupdate", () => {
           const progress = (currentAudio.currentTime / currentAudio.duration) * 100;
@@ -69,9 +93,17 @@ document.addEventListener("DOMContentLoaded", () => {
       if (isPlaying) {
         currentAudio.pause();
         isPlaying = false;
+        document.getElementById("music-title").textContent = "";
+        document.getElementById("music-image").setAttribute("src", "");
+        document.getElementById("music-artist").textContent = "";
+        const musicImageElement = document.getElementById("music-image");
+        musicImageElement.classList.add("hidden");
         currentAudio = null;
         playIcon.style.display = "inline";
         pauseIcon.style.display = "none";
+        progressBar.style.width = "0%";
+        timerDisplay.textContent = "0:00";
+
       }
     }
   });
@@ -81,13 +113,30 @@ document.addEventListener("DOMContentLoaded", () => {
       const clickPosition = (e.clientX - progressContainer.getBoundingClientRect().left) / progressContainer.clientWidth;
       const newTime = clickPosition * currentAudio.duration;
       currentAudio.currentTime = newTime;
-      updateProgressBar(currentAudio.currentTime);
     }
   });
+  
 
   volumeSlider.addEventListener("input", () => {
     if (currentAudio) {
       currentAudio.volume = volumeSlider.value;
+    }
+  });
+
+  volumeIcon.addEventListener("click", () => {
+    if (currentAudio) {
+      if (currentAudio.volume > 0) {
+        // Le son est actuellement activé, nous le désactivons
+        previousVolume = currentAudio.volume; // Sauvegarder le volume actuel
+        currentAudio.volume = 0;
+        volumeSlider.value = 0;
+        volumeIcon.src = "icons/mute.png";
+      } else {
+        // Le son était désactivé, nous le réactivons avec le volume précédent
+        currentAudio.volume = previousVolume; // Rétablir le volume précédent
+        volumeSlider.value = previousVolume; // Rétablir la valeur du slider
+        volumeIcon.src = "icons/sound.png";
+      }
     }
   });
 
@@ -102,4 +151,5 @@ document.addEventListener("DOMContentLoaded", () => {
     const seconds = Math.floor(time % 60);
     return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
   }
+
 });
--- main.js
+++ main.js
@@ -5,16 +5,15 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1280,
         height: 720,
-        minWidth: 1280,
-        minHeight: 720,
-        maxWidth: 1280,
-        maxHeight: 720,
+        maxWidth: 1920,
+        maxHeight: 1080,
+        icon: path.join(__dirname, 'app/icons/favicon.png'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
   
-    win.loadFile('app/index.html')
+    win.loadFile('app/main.html')
   }
 
   app.whenReady().then(() => {
@@ -24,3 +23,13 @@ const createWindow = () => {
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
   })
+
+  // Définir l'icône de l'application pour Windows
+  app.setAppUserModelId('com.example.myapp');
+
+  // Définir l'icône de l'application pour macOS
+  app.whenReady().then(() => {
+    if (process.platform === 'darwin') {
+      app.dock.setIcon(path.join(__dirname, 'icon.icns'));
+    }
+  });
